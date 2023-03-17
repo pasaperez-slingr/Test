602a38d2d5cbab48ccfb6c0f7e06a7bf3dbbf3fb
pasaperez <aperez@slingr.io>
2023-03-17 10:39:39 -0300

    21

---
+++ src/main/java/io/slingr/endpoints/test/Test.java
@@ -0,0 +1,23 @@
+package io.slingr.endpoints.test;
+
+import io.slingr.endpoints.HttpEndpoint;
+import io.slingr.endpoints.framework.annotations.ApplicationLogger;
+import io.slingr.endpoints.framework.annotations.SlingrEndpoint;
+import io.slingr.endpoints.services.AppLogs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SlingrEndpoint(name = "test")
+public class Test extends HttpEndpoint {
+
+    private Logger logger = LoggerFactory.getLogger(Test.class);
+
+    @ApplicationLogger
+    protected AppLogs appLogger;
+
+
+    @Override
+    public String getApiUri() {
+        return null;
+    }
+}
---
+++ src/test/java/io/slingr/endpoints/test/TestTest.java
@@ -0,0 +1,4 @@
+package io.slingr.endpoints.test;
+
+public class TestTest {
+}
