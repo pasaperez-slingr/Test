7c8c14ac266d8e8add0d88890c5a72fc335d9ef3
pasaperez <aperez@slingr.io>
2023-03-17 13:15:26 -0300

    24

--- src/test/java/io/slingr/endpoints/test/TestTest.java
+++ src/test/java/io/slingr/endpoints/test/TestTest.java
@@ -1,4 +1,70 @@
 package io.slingr.endpoints.test;
 
+import io.slingr.endpoints.services.exchange.Parameter;
+import io.slingr.endpoints.utils.Json;
+import io.slingr.endpoints.utils.tests.EndpointTests;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.*;
+
+@Ignore("For dev proposes")
 public class TestTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestTest.class);
+
+    private static EndpointTests test;
+
+    @BeforeClass
+    public static void init() throws Exception {
+        test = EndpointTests.start(new io.slingr.endpoints.test.Runner(), "test.properties");
+    }
+
+    @Test
+    public void testPostRequest() throws Exception {
+        // build request
+        final Json req = Json.map();
+        req.set("body", Json.map().set("field1", "A").set("field2", "B"));
+        req.set("path", "");
+        req.set("headers", Json.map().set("token", "abc"));
+
+        // test request
+        Json res = test.executeFunction("_post", req);
+        assertNotNull(res);
+        assertFalse(res.is(Parameter.EXCEPTION_FLAG));
+        // body is 'ok'
+        assertEquals("ok\n", res.string("body"));
+        // it is not a full response
+        assertFalse(res.is("fullResponse"));
+
+        // test request - full response
+        req.set("fullResponse", "true");
+        res = test.executeFunction("_post", req);
+        assertNotNull(res);
+        assertFalse(res.is(Parameter.EXCEPTION_FLAG));
+        // body is 'ok'
+        assertEquals("ok\n", res.string("body"));
+        // it has headers
+        assertNotNull(res.json("headers"));
+        assertFalse(res.json("headers").isEmpty());
+        // it has a 200 response code
+        assertEquals(200, (int) res.integer("status"));
+
+        // test request
+        req.set("body", Json.map().set("END", "ok"));
+        req.set("path", "");
+        req.set("headers", Json.map());
+        res = test.executeFunction("_post", req);
+        assertNotNull(res);
+        assertFalse(res.is(Parameter.EXCEPTION_FLAG));
+        // body is 'ok'
+        assertEquals("ok\n", res.string("body"));
+        // it is not a full response
+        assertFalse(res.is("fullResponse"));
+
+        logger.info("-- END");
+    }
 }
